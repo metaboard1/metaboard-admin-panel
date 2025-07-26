49c6a6c02bc5af66e19af3aeb35fb0da13dac0ec
Yash Kumar Jha <yashkumarjha20@gmail.com>
2025-07-26 18:52:35 +0530

    Update basePath.ts

--- src/constants/basePath.ts
+++ src/constants/basePath.ts
@@ -1,3 +1,3 @@
-// export const BASE_URL = 'https://api.metaboard.in';
-export const BASE_URL = 'http://localhost:9000';
+export const BASE_URL = 'https://api.metaboard.in';
+// export const BASE_URL = 'http://localhost:9000';
 export const BASE_ASSETS_URL = `${BASE_URL}/uploads`;
